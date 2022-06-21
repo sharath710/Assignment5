f9dcefd229dad25507ecda12d055eed3ddaf0e48
sharath710 <98634836+sharath710@users.noreply.github.com>
2022-06-21 11:41:08 +0530

    Update SwaggerConfig.java

--- spring-students/src/main/java/com/sharath/students/config/SwaggerConfig.java
+++ spring-students/src/main/java/com/sharath/students/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.sharath.students.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+@Profile("{!prod}")	//this disables the swagger in prod profile
 @Configuration
 @EnableSwagger2
 
